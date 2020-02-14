459b16cd5dfdede3b4586ca0bb154b72c7ad2c99
Noah Titarsole <noah.titarsole@student.hu.nl>
2020-02-14 12:06:00 +0100

    changed file path

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -19,14 +19,16 @@ int main(int argc, char * argv[]) {
 	//ImageFactory::setImplementation(ImageFactory::STUDENT);
 
 
-	ImageIO::debugFolder = "D:\\Users\\Rolf\\Downloads\\FaceMinMin";
+	ImageIO::debugFolder = "D:\\Users\\noaht\\Downloads\\FaceMinMin";
 	ImageIO::isInDebugMode = true; //If set to false the ImageIO class will skip any image save function calls
 
 
 
 
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("D:\\Users\\Rolf\\Downloads\\TestA5.jpg", *input)) {
+	if (!ImageIO::loadImage(
+		"D:\\Users\\noaht\\Documents\\GitHub\\HU-Vision-1819-Vision-Rick-van-Mourik-Noah-Titarsole\\testsets\\Set A\\TestSet Images\\male-1.png", 
+		*input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;

e11daefd0ad4aa2ac7068f3ddb596b299268c63f
Rick van Mourik <rick.vanmourik@student.hu.nl>
2020-03-20 17:01:10 +0100

    fixing gui

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -19,16 +19,14 @@ int main(int argc, char * argv[]) {
 	//ImageFactory::setImplementation(ImageFactory::STUDENT);
 
 
-	ImageIO::debugFolder = "D:\\Users\\noaht\\Downloads\\FaceMinMin";
+	ImageIO::debugFolder = "D:\\github\\HU-Vision-1819-Vision-Rick-van-Mourik-Noah-Titarsole\\source\\ExternalDLL\\Debug";
 	ImageIO::isInDebugMode = true; //If set to false the ImageIO class will skip any image save function calls
 
 
 
 
-	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage(
-		"D:\\Users\\noaht\\Documents\\GitHub\\HU-Vision-1819-Vision-Rick-van-Mourik-Noah-Titarsole\\testsets\\Set A\\TestSet Images\\male-1.png", 
-		*input)) {
+	RGBImage* input = ImageFactory::newRGBImage();
+	if (!ImageIO::loadImage("D:\\github\\HU-Vision-1819-Vision-Rick-van-Mourik-Noah-Titarsole\\testsets\\Set A\\TestSet Images\\male-1.png", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
